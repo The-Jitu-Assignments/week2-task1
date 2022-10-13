fe95f460f204a0cc4be286bd700d4c973576fb82
john katua <johnkatua99@gmail.com>
2022-10-13 08:26:38 +0300

    removed the console.log from app.js file

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const fetchData = async (url) => {
   try {
     let res = await fetch(url)
     let data = await res.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.log(error)
